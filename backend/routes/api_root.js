4ddc1fa304ad57394348ba3a963d16c22ca7a2fc
jaro <jaro.bachenberg@stud.hs-kempten.de>
2026-06-11 09:40:35 +0200

    fix: changelog API public (remove auth middleware from GET)

--- backend/routes/api_root.js
+++ backend/routes/api_root.js
@@ -41,7 +41,7 @@ router.use("/leaderboard", auth, leaderboardRoutes);
 router.use("/users", auth, usersRoutes);
 router.use("/messages", auth, messagesRoutes);
 router.use("/bugs", auth, bugsRoutes);
-router.use("/changelog", auth, changelogRoutes);
+router.use("/changelog", changelogRoutes);
 router.use("/admin", auth, adminRoutes);
 
 // Health Check
